6497cacbd2161cfffc160f8f7d36a24edce4ba99
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-21 17:03:53 -0700

    wrote first POST test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,15 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('submits a new user and gives them a pokemon', async () => {
+    const res = await request(app)
+      .post('/api/v1/users')
+      .send({
+        name: 'Chase',
+        catchPhrase: 'got em!'
+      });
+
+    expect(res.body).toEqual({ id: 1, name: 'Chase', catchPhrase: 'got em!' });
+  });
 });
