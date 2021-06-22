d1fb0d22b6a218a8c949be7010027c36ff71154a
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-06-21 22:31:05 -0700

    makes a number from a name but numbers are too big

--- __tests__/service.test.js
+++ __tests__/service.test.js
@@ -1,6 +1,6 @@
 import Pokedex from 'pokedex-promise-v2';
 
-describe('is the API working?', () => {
+describe('Pokemon API and associated functions', () => {
 
   it('Calls to the API to get a pokemon', async () => {
     const P = new Pokedex();
@@ -10,5 +10,23 @@ describe('is the API working?', () => {
 
         expect(response.name).toEqual('eevee');
       });
+  });
+
+  it('Calls to the API to get a pokemon by number', async () => {
+    const P = new Pokedex();
+
+    P.getPokemonByName('12')
+      .then((response) => {
+
+        expect(response.name).toEqual('butterfree');
+      });
+  });
+
+  it('Takes in a name and produces a number', () => {
+    const name = 'nick';
+
+    const number = parseInt(name, 36);
+
+    expect(number).toBe(37);
   });
 });
